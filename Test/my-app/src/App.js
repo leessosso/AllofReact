b7a8136fbd083da2040fbdeaa98432c9b6e73819
LEE Seungseok <ssossoking@naver.com>
2021-10-07 17:44:16 +0900

    20211007 from laptop

--- Test/my-app/src/App.js
+++ Test/my-app/src/App.js
@@ -7,18 +7,28 @@ import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 import Event from './components/2-7.Event/Event';
 import Condition from './components/2-8.ConditionalRendering/Condition';
+import Reducer from './components/3-3.Hooks/Reducer';
+import State from './components/3-3.Hooks/State';
+import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+import Dialog from './components/3-6.Composition2/Dialog';
+import ThankyouDialog from './components/3-6.Composition2/ThankyouDialog';
 
 function App() {
   return (
     <div className="App">
-      <ControlledComponent />
-      {/* <Condition />
-      <Event/>
-      <Composition />
-      <Extraction />
-      <ClassComponent />
-      <FunctionalComponent />
-      <ClassComponent2 /> */}
+      <ThankyouDialog />
+      {/* <Dialog /> */}
+      {/* <WelcomeDialog /> */}
+      {/* <Reducer /> */}
+      {/* <State /> */}
+      {/* <ControlledComponent /> */}
+      {/* <Condition /> */}
+      {/* <Event/> */}
+      {/* <Composition /> */}
+      {/* <Extraction /> */}
+      {/* <ClassComponent /> */}
+      {/* <FunctionalComponent /> */}
+      {/* <ClassComponent2 />  */}
     </div>
   );
 }
---
+++ Test/my-app/src/components/3-5.Composition/WelcomeDialog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CustomDialog from './CustomDialog'
+import Dialog from './Dialog'
+
+export default function WelcomeDialog() {
+    return (
+        // <Dialog>
+        //     <h1>Welcome</h1>
+        //     <h5>Thank you!!</h5>
+        // </Dialog>
+        <CustomDialog title="Welcome CustomDialog" description="Thanks" />
+    )
+}

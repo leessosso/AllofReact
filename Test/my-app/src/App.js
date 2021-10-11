5df534600ba47465713f6c519d43a41af9b8e871
LEE Seungseok <ssossoking@naver.com>
2021-10-11 11:26:40 +0900

    20211011 from laptop

--- Test/my-app/src/App.js
+++ Test/my-app/src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
-import ControlledComponent from './components/2-10.Form/ControlledComponent';
-import Composition from './components/2-4.Props/Composition';
-import Extraction from './components/2-4.Props/Extraction/Extraction';
-import ClassComponent from './components/2-5.State/ClassComponent';
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
-import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
-import Event from './components/2-7.Event/Event';
-import Condition from './components/2-8.ConditionalRendering/Condition';
-import Reducer from './components/3-3.Hooks/Reducer';
-import State from './components/3-3.Hooks/State';
-import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
-import Dialog from './components/3-6.Composition2/Dialog';
-import ThankyouDialog from './components/3-6.Composition2/ThankyouDialog';
+// import ControlledComponent from './components/2-10.Form/ControlledComponent';
+// import Composition from './components/2-4.Props/Composition';
+// import Extraction from './components/2-4.Props/Extraction/Extraction';
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+// import Event from './components/2-7.Event/Event';
+// import Condition from './components/2-8.ConditionalRendering/Condition';
+// import Reducer from './components/3-3.Hooks/Reducer';
+// import State from './components/3-3.Hooks/State';
+// import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+// import Dialog from './components/3-6.Composition2/Dialog';
+// import ThankyouDialog from './components/3-6.Composition2/ThankyouDialog';
+import Button from './components/3-7.HOC/Button';
+import Input from './components/3-7.HOC/input';
 
 function App() {
   return (
     <div className="App">
-      <ThankyouDialog />
+      <Input />
+      <br />
+      <br />
+      <Button />
+      {/* <ThankyouDialog /> */}
       {/* <Dialog /> */}
       {/* <WelcomeDialog /> */}
       {/* <Reducer /> */}
